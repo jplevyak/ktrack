ca0e0b1525ce778ea6ae2b704008257396a65b7e
John Plevyak <jplevyak@gmail.com>
2022-10-08 10:45:59 -0700

    Fix.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm run build
-rsync -av --delete --exclude .svelte-kit --exclude /data --exclude /today --exclude /favorites --exclude /history --exclude /profile --exclude /.git . ktrack.org:ktrack
+rsync -av --delete --exclude /data --exclude /today --exclude /favorites --exclude /history --exclude /profile --exclude /.git . ktrack.org:ktrack
 ssh ktrack.org "(cd ktrack; ./restart_ktrack.sh)"
