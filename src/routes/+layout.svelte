e3c5c2d82963e4d2f13a70089cb9f21202775f09
John Plevyak <jplevyak@gmail.com>
2022-10-08 11:26:15 -0700

    Add swipe.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,37 @@
 <script>
-  import { onDestroy } from 'svelte';
+	import { onDestroy } from 'svelte';
   import { page } from '$app/stores'
-	import Nav from '../components/Nav.svelte';
+  import Nav from '../components/Nav.svelte';
+  import { swipe } from 'svelte-gestures';
+  import { goto } from '$app/navigation'
 
-	export let segment;
+export let segment;
 
-  const unsubscribe = page.subscribe(value => {
-    segment = value.url.pathname.slice(1).split('/')[0];
-  });
-  onDestroy(unsubscribe);
+const unsubscribe = page.subscribe(value => {
+	segment = value.url.pathname.slice(1).split('/')[0];
+});
+onDestroy(unsubscribe);
+
+const segments = ['', 'favorites', 'search', 'history', 'about'];
+
+function handle_swipe(event) {
+  if (event.detail.direction == 'left') {
+    let i = segments.indexOf(segment);
+    if (i >= 0) {
+    	i += 1;
+    	if (i > 4) i = 0;
+    	goto('/' + segments[i]);
+    }
+  } else if (event.detail.direction == 'right') {
+		let i = segments.indexOf(segment);
+  	if (i >= 0) {
+  		i -= 1;
+			if (i < 0) i = 4;
+   	  goto('/' + segments[i]);
+ 		}
+ 	}
+}
+   
 </script>
 
 <style>
@@ -21,8 +44,10 @@
 	}
 </style>
 
+<div use:swipe={{ timeframe: 300, minSwipeDistance: 100}} on:swipe={handle_swipe}>
 <Nav {segment}/>
 
 <main>
 	<slot></slot>
 </main>
+</div>
