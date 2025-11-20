299bd18c490c291b6408d4e7c419e3f09b8cb8cb
John Plevyak <jplevyak@gmail.com>
2025-11-20 11:34:36 -0800

    fix: disable tombstone pruning in favorites endpoint

--- src/routes/api/favorites/+server.js
+++ src/routes/api/favorites/+server.js
@@ -1,10 +1,9 @@
 import LevelPkg from "level";
 const { Level } = LevelPkg;
 import { do_post } from "../../_post.js";
-import { prune_tombstones } from "../../_util.js";
 
 var favorites = new Level("./favorites");
 
 export async function POST(req) {
-  return await do_post(req, favorites, prune_tombstones);
+  return await do_post(req, favorites, x => x);
 }
